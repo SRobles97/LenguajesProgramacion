e01b1759690a91c21ae97a18b1c09c8d8d84ae94
SRobles97 <37591168+SRobles97@users.noreply.github.com>
2020-09-24 16:49:06 -0300

    Add files via upload

---
+++ Actividades/python/actividad1.py
@@ -0,0 +1,13 @@
+print("Hola Mundo")
+nombre = "Noelle!"
+print("Hola ",nombre)
+print("Hola "+nombre)
+numero = 42
+print("Hola ",numero)
+print("Hola "+str(numero))
+alimentoUno = "sushi"
+alimentoDos = "pizza"
+print("Me encanta comer {} y {}.".format(alimentoUno,alimentoDos))
+frase = "Me encanta comer %s" % "sushi y pizza."
+print(frase)
+print(f"Me encanta comer {alimentoUno} y {alimentoDos}.")

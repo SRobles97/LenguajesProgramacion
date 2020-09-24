e56f4d241b4ad270cd82659b62e9d2361064b74f
SRobles97 <37591168+SRobles97@users.noreply.github.com>
2020-09-24 18:01:39 -0300

    Add files via upload

---
+++ Actividades/python/for_loop_basic1.py
@@ -0,0 +1,22 @@
+for a in range(151):
+        print(a)
+for b in range(5,1000,5):
+        print(b)
+for c in range(100):
+        if(c % 5 == 0):
+            print("Hola")
+        if(c % 10 == 0):
+            print("Hola Mundo")
+resultado = 0
+for d in range(500000):
+        if(d % 2 != 0):
+            resultado = resultado+d
+print(resultado)
+for e in range(2018,0,-4):
+        print(e)
+lowNum = 1
+highNum = 30 
+mult = 5
+for f in range(lowNum,highNum+1,1):
+        if(f % mult == 0):
+            print(f)
